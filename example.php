250cf4633aba3fe5be55a11ebdc25c63896fa050
Ciaran Synnott <hello@synnott.co.uk>
2015-11-24 15:28:17 +0000

    Create example.php

---
+++ example.php
@@ -0,0 +1,34 @@
+<?php
+
+include "OBDIIHelper.php";
+use OBDIIHelper\OBDIIHelper;
+
+$o = new OBDIIHelper();
+
+$code = $o->get_code_data("P1448");
+
+/* OUTPUT for P1448
+
+      stdClass Object
+      (
+          [code] => P1448
+          [code_type] => Manufacturer defined
+          [system_group] => Powetrain
+          [system_area] => Emission control
+      )
+      
+*/
+
+$code = $o->get_code_data("P2248");
+
+/* OUTPUT for P2248
+
+    stdClass Object
+    (
+        [code] => P2248
+        [code_type] => SAE defined (EOBD)
+        [system_group] => Powetrain
+        [system_area] => Fuel, air or emission control
+  )
+
+*/
